d88ef768277311b1fa9afda30721cd5adbee33c0
SHIKHAR-Bajpai <bajpaishikhar120@gmail.com>
2025-07-26 16:19:02 +0530

    added docker file

--- backend/app.py
+++ backend/app.py
@@ -732,4 +732,5 @@ def add_pose():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, threaded=True)
+    # app.run(debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8080)
