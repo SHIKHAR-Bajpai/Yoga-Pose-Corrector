1ec948e728b8a8f88886455693fb8e5a2d496195
SHIKHAR-Bajpai <bajpaishikhar120@gmail.com>
2025-07-26 21:25:45 +0530

    updates

--- backend/app.py
+++ backend/app.py
@@ -732,5 +732,4 @@ def add_pose():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    # app.run(debug=True, threaded=True)
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True, threaded=True)
